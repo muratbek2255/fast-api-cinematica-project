4bfe724c9255acb3674831276bac828c3145f7a7
muratbek2255 <muratbek2255@gmail.com>
2023-05-03 12:54:46 +0600

    add new project

--- apps/routes.py
+++ apps/routes.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from apps.cinema.api import cinema_router
 from apps.movie.api import movie_router
+from apps.rent.api import rent_router
 
 api_router = APIRouter()
 
 api_router.include_router(cinema_router, prefix='/cinema', tags=["cinema"])
 api_router.include_router(movie_router, prefix='/movie', tags=["movie"])
+api_router.include_router(rent_router, prefix='/rent', tags=["rent"])
--- core/local_config.py
+++ core/local_config.py
@@ -1,4 +1,4 @@
 POSTGRES_USER = 'your_username'
 POSTGRES_PASSWORD = 'your_password'
 POSTGRES_SERVER = 'your_host'
-POSTGRES_DB = 'your_db_name'
+POSTGRES_DB = 'your_db'
