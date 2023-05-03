dcca91cb5f6a1a10978661891529912350828e9d
muratbek2255 <muratbek2255@gmail.com>
2023-05-03 13:07:22 +0600

    fix in rent/repository

--- apps/rent/repository.py
+++ apps/rent/repository.py
@@ -19,14 +19,14 @@ class RentRepository:
         if rent.status == StatusOfRent.BOOKED:
             return "Rent is in been not free"
         else:
-            await rent.update(status=StatusOfRent.IN_PROGRESS)
+            await rent.update(status="IN_PROGRESS")
             return "Status in Progress"
 
     async def rollback_rent(self, id: int):
 
         rent = await self.get_movie(id)
 
-        if rent.status == StatusOfRent.IN_PROGRESS:
-            rent.update(is_rent=False)
-            rent.update(status=StatusOfRent.FREE)
+        if rent.status == "IN_PROGRESS":
+            await rent.update(is_rent=False)
+            await rent.update(status="FREE")
             return "Rent rollback"
